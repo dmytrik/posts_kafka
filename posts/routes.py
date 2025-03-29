0f84a03426416a12aa61b071206c3034c34108bd
dominic <dominic@gotyourbacksupport.com>
2025-03-29 16:20:00 +0200

    feat: add producer

--- posts/routes.py
+++ posts/routes.py
@@ -1,3 +1,6 @@
+import json
+
+from aiokafka import AIOKafkaProducer
 from fastapi import APIRouter, Depends, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -5,6 +8,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from schemas import PostRequestSchema, PostResponseSchema
 from models import PostModel
 from database import get_db
+from producer import get_kafka_producer
 
 router = APIRouter()
 
@@ -16,12 +20,20 @@ async def get_posts(db: AsyncSession = Depends(get_db)):
 
 
 @router.post("/", response_model=PostResponseSchema, status_code=status.HTTP_201_CREATED)
-async def create_post(post: PostRequestSchema, db: AsyncSession = Depends(get_db)):
-    post = PostModel(
+async def create_post(
+        post: PostRequestSchema,
+        db: AsyncSession = Depends(get_db),
+        producer: AIOKafkaProducer = Depends(get_kafka_producer)
+):
+    new_post = PostModel(
         content=post.content,
         author=post.author
     )
-    db.add(post)
+    db.add(new_post)
     await db.commit()
-    await db.refresh(post)
-    return post
+    await db.refresh(new_post)
+    await producer.send_and_wait(
+        topic="post_created",
+        value=json.dumps(new_post.__dict__)
+    )
+    return new_post
